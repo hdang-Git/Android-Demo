cf4b00c59ba96d319ed7bc41f81d12202ddf1161
hdang-Git <hdang0010@gmail.com>
2017-03-10 10:16:37 -0500

    Working demo of sliding tab layout with fragment communication

---
+++ SlidingTabNav/app/src/main/java/com/example/hai/slidingtabnav/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.hai.slidingtabnav;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.logging.Logger;
+
+public class MainActivity extends AppCompatActivity implements Tab1.SenderInterface {
+    TabLayout tabLayout;
+    FragmentViewPagerAdapter adapter;
+    Logger log = Logger.getAnonymousLogger();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Get the ViewPager and set it's PagerAdapter so that it can display items
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        adapter = new FragmentViewPagerAdapter(getSupportFragmentManager(),
+                MainActivity.this);
+        viewPager.setAdapter(adapter);
+        // Give the TabLayout the ViewPager
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+    }
+
+    @Override
+    public void passNumber(int num) {
+        Tab2 fragment = (Tab2) adapter.getFragment(1);
+        if(fragment != null){
+            log.info("fragment isn't null");
+            fragment.setNumber(num);
+        } else {
+            Log.d("Not Initialized", "Fragment 2 isn't initialized");
+        }
+    }
+}
